cf0b279f3fa0062df43a3f68ea4cfcfcbbde85f3
Luiggi Mendoza <luiggi0925@gmail.com>
2018-06-24 00:16:59 -0500

    Borré el código porque yolo.

--- src/java/Martillito/modelo/Producto.java
+++ src/java/Martillito/modelo/Producto.java
@@ -6,32 +6,5 @@
 package Martillito.modelo;
 
 public class Producto {
-    private String cod_prod;
-    private String nom_prod;
-    private double precio;
-    private int stock;
-
-    public Producto(String cod_prod, String nom_prod, double precio, int stock) {
-        this.cod_prod = cod_prod;
-        this.nom_prod = nom_prod;
-        this.precio = precio;
-        this.stock = stock;
-    }
-
-    public String getCod_prod() {
-        return cod_prod;
-    }
-
-    public String getNom_prod() {
-        return nom_prod;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public int getStock() {
-        return stock;
-    }
     
 }
